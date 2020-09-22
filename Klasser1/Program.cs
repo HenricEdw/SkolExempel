dd1c1f42df52914fdc191f27f14e3543a49dd402
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-09-22 12:26:42 +0200

    Uploaded initial files
    Added first commit

---
+++ Klasser1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bil minBil = new Bil("Svart", "5", "Seat", "AAA 111", 180);//Här skapas en INSTANS av klassen Bil! En SPECIFIK bil. Min Seat i detta fall
+
+            //Bil larsBil = new Bil();
+            
+            Console.WriteLine(minBil.Drive());
+
+
+            //Ett annat sätt att instatsiera genom att tilldela världen direkt 
+            Bil minAndraBil = new Bil("Silver Metallic", "5", "Hyundai", "BBB 222", 80);
+
+            Console.WriteLine();//Lite extra space bara
+
+            Console.WriteLine($"Min {minAndraBil.GetModel()} är inte lika snabb som min {minBil.GetModel()}. Den går bara i {minAndraBil.GetSpeed()}, " +
+                $"men jag kan iaf ropa på Drive! {minAndraBil.Drive()}");
+
+            //Bokens Random-exempel
+            //Random rand = new Random();
+            //int randomNumber = rand.Next();
+
+            //Console.WriteLine($"Här kommer ett slumptal {randomNumber}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Klasser1/bil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser1
+{
+    class Bil
+    {
+        private string colour;
+        private string numberOfDoors;
+        private string model;
+        private string regNumber;
+        private int speed;
+
+        public Bil(string colour, string numberOfDoors, string model, string regNumber, int speed)
+        {
+            this.colour = colour;
+            this.model = model;
+            this.numberOfDoors = numberOfDoors;
+            this.regNumber = regNumber;
+            this.speed = speed;
+        }
+
+        public string Drive()
+        {
+            return $"Jag kör just nu i {speed} km/h!";
+        }
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public int GetSpeed()
+        {
+            return speed;
+        }
+    }
+}
